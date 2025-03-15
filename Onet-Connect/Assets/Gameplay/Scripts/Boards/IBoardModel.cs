0a46ade19b43845ce1a1eadbaa8a20c290194f4a
Titan Tran <t.p.tai93@gmail.com>
2025-03-15 11:59:51 +0700

    + Add new assets, refactoring structure projects

---
+++ Onet-Connect/Assets/Gameplay/Scripts/Boards/IBoardModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TGS.OnetConnect.Gameplay.Scripts.Tiles;
+
+namespace TGS.OnetConnect.Gameplay.Scripts.Boards
+{
+    public interface IBoardModel
+    {
+        // TODO: TileModel => ITileModel
+        TileModel[,] TileModelArray { get; }
+        BoardTunables Tunables { get; }
+        void SetTileArray(TileModel[,] tiles);
+    }
+
+    public abstract class BoardModelBase : IBoardModel, IDisposable
+    {
+        public abstract TileModel[,] TileModelArray { get; }
+        public abstract BoardTunables Tunables { get; }
+        public abstract void SetTileArray(TileModel[,] tiles);
+        public abstract void Dispose();
+    }
+}
